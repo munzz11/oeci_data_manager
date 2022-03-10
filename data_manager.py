b184c7fe95eaa9779ccf9881f126e80e53f6940f
Roland Arsenault <roland@ccom.unh.edu>
2022-03-10 11:34:31 -0500

    Improve progress feedback

--- data_manager.py
+++ data_manager.py
@@ -20,6 +20,14 @@ def get_hash(filename):
       datasize += len(chunk)
   return hash.hexdigest(), datasize
 
+# from https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
+def human_readable_size(size, decimal_places=3):
+    for unit in ['B','KiB','MiB','GiB','TiB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f}{unit}"
+
 if len(sys.argv) < 2:
   usage()
 
@@ -63,8 +71,6 @@ for platform in platforms:
       print ('        sensor:', sensor.parts[-1])
 
 files = []
-file_info = []
-total_size = 0
 start_time = datetime.datetime.now()
 
 manifest = []
@@ -74,42 +80,71 @@ for potential_file in top_level.glob("**/*"):
     if potential_file.is_file():
       files.append(potential_file)
 
-count = 0
-next_report = 0
-for i in range(len(files)):
-  json_file = data_manager_dir/files[i].relative_to(top_level).parent/(files[i].name+'.json')
-  if json_file.is_file():
-    config = json.load(open(json_file))
+configs = {}
+total_size = 0
+need_hash_size = 0
+need_hash_count = 0
+
+
+for f in files:
+  configs[f] = {}
+  configs[f]['json_file'] = data_manager_dir/f.relative_to(top_level).parent/(f.name+'.json')
+  if configs[f]['json_file'].is_file():
+    config = json.load(open(configs[f]['json_file']))
   else:
     config = {}
 
-  file_size = files[i].stat().st_size
-  mod_time = files[i].stat().st_mtime
+  file_size = f.stat().st_size
+  mod_time = f.stat().st_mtime
 
   need_hash = not ('size' in config and config['size'] == file_size and 'modify_time' in config and config['modify_time'] == mod_time)
+  if need_hash or not 'hash' in config:
+    config['hash'] = None
 
-  if need_hash:
-    #print('hashing', files[i])
-    h,s = get_hash(files[i])
-    config['hash'] = h
-    config['size'] = file_size
-    config['modify_time'] = mod_time
-    json_file.parent.mkdir(parents=True, exist_ok=True)
-    json.dump(config, open(json_file,"w"))
+  config['size'] = file_size
+  config['modify_time'] = mod_time
+  total_size += file_size
+  if config['hash'] is None:
+    need_hash_size += file_size
+    need_hash_count += 1
 
-    file_info.append((files[i],h,s))
-    total_size += s
-    count += 1
+  configs[f]['config'] = config
 
-  manifest.append((files[i].relative_to(top_level),config['hash']))
-  if count >= next_report:
-    next_report += 50
-    time_so_far = datetime.datetime.now()-start_time
-    print (count,'of',len(files),' files, seconds elapsed:', time_so_far.total_seconds(), 'rate:', total_size/time_so_far.total_seconds(),'bytes per second')
+start_hash_time = datetime.datetime.now()
+print ('scan time:', start_hash_time-start_time)
+
+print (len(files),'files totaling',human_readable_size(total_size),'bytes')
+print (need_hash_count,'need hashing totaling',human_readable_size(need_hash_size),'bytes')
+
+newly_hashed_count = 0
+newly_hashed_size = 0
+
+last_report_time = start_hash_time
+last_report_newly_hashed_size = 0
+
+for f in files:
+  if configs[f]['config']['hash'] is None:
+    h,s = get_hash(f)
+    configs[f]['config']['hash'] = h
+    configs[f]['json_file'].parent.mkdir(parents=True, exist_ok=True)
+    json.dump(configs[f]['config'], open(configs[f]['json_file'],"w"))
+    newly_hashed_count += 1
+    newly_hashed_size += configs[f]['config']['size']
+    now = datetime.datetime.now()
+    since_last_report = now-last_report_time
+    if since_last_report > datetime.timedelta(seconds=5):
+      hash_rate = (newly_hashed_size-last_report_newly_hashed_size)/since_last_report.total_seconds()
+      estimated_time_remaining = (need_hash_size - newly_hashed_size) / hash_rate
+      percentage_complete = 100*newly_hashed_size/need_hash_size
+      print("percent complete:", percentage_complete,"rate:",human_readable_size(hash_rate)+'/s',"estimated seconds remaining:", estimated_time_remaining)
+      last_report_time = now
+      last_report_newly_hashed_size = newly_hashed_size
+  manifest.append((f.relative_to(top_level),configs[f]['config']['hash']))
+
 
 end_time = datetime.datetime.now()
 
-print("hashed",len(file_info), 'files totaling', total_size, 'bytes in', (end_time-start_time).total_seconds(), ' seconds.')
+print("hashed", newly_hashed_count, 'files totaling', human_readable_size(newly_hashed_size), ' duration:', (end_time-start_hash_time))
 
 manifest_file = open(top_level/"manifest_temp.txt",'w')
 for f in manifest:
