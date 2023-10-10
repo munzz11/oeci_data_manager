fdd8d366dfabdd34f8c009ed7e4dacfa4e796bf7
Field User <field@ccom.unh.edu>
2023-10-10 06:18:39 +0000

    Fixed bugs. Added args and comments.

--- utility_scripts/data_pull.sh
+++ utility_scripts/data_pull.sh
@@ -24,25 +24,28 @@ ARCHIVE='/home/field/data/mnt/external_drive/archive'
 
 LINK=$1
 
-if [ "${LINK}" = '' -o `echo "${LINK}" | grep wire` ]
+if [ `echo ${LINK} | grep dryrun` ]
+then
+    DRYRUN='--dry-run'
+    LINK=`echo "${LINK}" | sed 's/dryrun//g'`
+else
+    DRYRUN=''
+fi
+
+if [ "${LINK}" == '' ] || [ `echo "${LINK}" | grep wire` ]
 then
     ECHOIP='192.168.8.180'
-else if [ `echo ${LINK} | grep mbr` ]
+elif [ `echo ${LINK} | grep mbr` ]
 then
     ECHOIP='172.16.11.8'
     ECHOPORT='13001'
-else if [ '`echo ${LINK} | grep wifi' ]
+elif [ `echo ${LINK} | grep wifi` ]
 then
     ECHOIP='172.16.10.8'
     ECHOPORT='13001'
 fi
 
-if [ `echo ${LINK} | grep dryrun`]
-then
-    DRYRUN='--dry-run'
-else
-DRYRUN=''
-fi
+
 
 # A failed experiment to filter data by date/time so we only archive
 # data while deployed. Could not figure out how to get the paths right
@@ -53,12 +56,14 @@ fi
 #echo ${FILES2TRANSFER} | rsync -ravPz ${DRYRUN} --files-from=- ${ECHOIP}:/ ${RAW}/
 # ----
 
-if [ "${LINK} = '' -o `echo "${LINK}" | grep wire ]
+if [ "${LINK}" = '' ] || [ `echo "${LINK}" | grep wire` ]
 then
    # Use this one for transfers via a direct connection to DriX.
+   echo "Pulling data directly over the wire."
    rsync -ravPz ${DRYRUN} ${ECHOIP}:${ARCHIVE}/ ${RAW}/
 else
 
    # Use this one for transfers over wifi or mbr:
-   rsync -ravPz -e "ssh -p ${ECHOPORT}" ${DRYRUN} ${ECHOIP}:${ARCHIVE} ${RAW}/
+   echo "Pulling data by mbr or wifi"
+   rsync -ravPz -e "ssh -p ${ECHOPORT}" ${DRYRUN} ${ECHOIP}:${ARCHIVE}/ ${RAW}/
 fi
